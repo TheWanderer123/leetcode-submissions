9ab8c502b36301946ad65b59c6239498315a5118
TheWanderer123 <tranngocphuc31102007@gmail.com>
2026-07-24 13:51:03 +0700

    Time: 5 ms (67.38%) | Memory: 17.5 MB (73.21%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.cpp
+++ 965-unique-email-addresses/unique-email-addresses.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
         unordered_set<string> cleans;
-        for (const string& s: emails){
-            size_t posat=s.find('@');
-            string loc=s.substr(0,posat);
+        // for (const string& s: emails){
+        //     size_t posat=s.find('@');
+        //     string loc=s.substr(0,posat);
+        //     string clean="";
+        //     for (const char& c:loc){
+        //         if(c=='+') break;
+        //         else if(c=='.') continue;
+        //         else clean+=c;
+        //     }
+        //     string dom=s.substr(posat);
+        //     clean+=dom;
+        //     cleans.insert(clean);
+        // }
+        // return cleans.size();
+        for(const string& email:emails){
             string clean="";
-            for (const char& c:loc){
-                if(c=='+') break;
-                else if(c=='.') continue;
-                else clean+=c;
+            int i=0;
+            while (email[i]!='@'){
+                if(email[i]=='.'){
+                    ++i;continue;
+                }
+                if(email[i]=='+'){
+                    while (email[i]!='@') ++i;
+                    break;
+                }
+                clean+=email[i];
+                ++i;
             }
-            string dom=s.substr(posat);
-            clean+=dom;
+            clean+=email.substr(i);
             cleans.insert(clean);
         }
         return cleans.size();
