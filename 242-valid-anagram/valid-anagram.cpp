4d427f724cb3cf2e3ae89fbcfaccce001d257441
TheWanderer123 <tranngocphuc31102007@gmail.com>
2026-08-01 13:46:07 +0700

    Time: 0 ms (100.00%) | Memory: 9.7 MB (76.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end()); sort(t.begin(),t.end());
-        return (s==t);
+        if(s.size()!=t.size()) return 0;
+        int count[26]={0};
+        for(int i=0;i<s.size();++i){
+            ++count[s[i]-'a'];
+            --count[t[i]-'a'];
+        }
+        for(int i=0;i<26;++i){
+            if(count[i]!=0) return 0;
+        }
+        return 1;
     }
 };
