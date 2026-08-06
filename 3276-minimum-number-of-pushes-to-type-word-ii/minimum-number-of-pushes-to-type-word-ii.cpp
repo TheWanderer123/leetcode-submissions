e0e6a3aa1642e872ce507f6200cbf8fdd5d45a1c
TheWanderer123 <tranngocphuc31102007@gmail.com>
2026-08-06 13:05:04 +0700

    Time: 33 ms (16.42%) | Memory: 29.4 MB (5.41%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int>freq(26,0);
+        // vector<int>freq(26,0);
+        // int total=0;
+        // for(char c:word) ++freq[c-'a'];
+        // sort(freq.rbegin(),freq.rend());// the biggest in front
+        // for(int i=0;i<26;++i){
+        //     if(freq[i]==0) break;
+        //     int push=(i/8)+1;
+        //     total+=push*freq[i];
+        // }
+        // return total;
         int total=0;
-        for(char c:word) ++freq[c-'a'];
-        sort(freq.rbegin(),freq.rend());// the biggest in front
-        for(int i=0;i<26;++i){
+        unordered_map<char,int>freqmap;
+        for(char c:word) ++freqmap[c];
+        vector<int>freq;
+        for(auto&[c,f]:freqmap) freq.push_back(f);
+        sort(freq.rbegin(),freq.rend()); // the biggest in front
+        for(int i=0;i<freq.size();++i){
             if(freq[i]==0) break;
             int push=(i/8)+1;
             total+=push*freq[i];
